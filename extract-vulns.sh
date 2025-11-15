4476b2ec98be5e990030ca2dd7d4d2b1489c2738
manojxshrestha <106522935+manojxshrestha@users.noreply.github.com>
2025-11-15 11:00:35 +0545

    Updated extract-vulns.sh

--- extract-vulns.sh
+++ extract-vulns.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-patterns=("debug_logic" "img-traversal" "interestingparams" "jsvar" "rce" "sqli" "ssti" "idor" "interestingEXT" "interestingsubs" "lfi" "redirect" "ssrf" "xss")
+# Find all .json files in ~/.gf/ and extract basenames as patterns
+patterns=($(ls ~/.gf/*.json | xargs -n1 basename | sed 's/\.json$//'))
 for pat in "${patterns[@]}"; do
     output="${pat//_/-}-urls.txt"
     gf "$pat" crawledurls.txt > "$output"
